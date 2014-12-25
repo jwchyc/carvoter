5716e4dd7d2ac8ded92d40ec66b162c601ccc3d3
jwchyc <jwchyc@hotmail.com>
2014-12-25 12:48:06 -0500

    Initial Commit

---
+++ CarVoter/Viewmodel/OverviewViewmodel.cs
@@ -0,0 +1,14 @@
+﻿using CarVoter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarVoter.Viewmodel
+{
+    public class OverviewViewmodel
+    {
+        public IList<Car> Cars { get; set; }
+    }
+}
